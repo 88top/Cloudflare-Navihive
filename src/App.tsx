2f380b738a3aebf8dc0654f2887bb4ce297d16b3
timeflysoon <88620233+timeflysoon@users.noreply.github.com>
2026-08-15 22:39:06 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,6 @@ import {
 } from '@mui/material';
 import SortIcon from '@mui/icons-material/Sort';
 import SaveIcon from '@mui/icons-material/Save';
-import CancelIcon from '@mui/icons-material/Cancel';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
